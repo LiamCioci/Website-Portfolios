cbe6731832ab2e1d249da4e506da5aa74aacebb0
LiamCioci <132361031+LiamCioci@users.noreply.github.com>
2023-05-02 15:41:19 +0000

    psuh

--- js/three.js
+++ js/three.js
@@ -437,3 +437,17 @@ function init(event){
   createBall();
   loop();
 }
+
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+if ( WebGL.isWebGLAvailable() ) {
+
+	// Initiate function or other initializations here
+	animate();
+
+} else {
+
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
+}
