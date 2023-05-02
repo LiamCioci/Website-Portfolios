19005cd08c6771db465e42534aa1d018f41b3bb1
LiamCioci <132361031+LiamCioci@users.noreply.github.com>
2023-05-02 13:35:30 +0000

    Adding Fun Space Section

---
+++ js/fun-space scripts.js
@@ -0,0 +1,12 @@
+<!-- 
+      3D Portfolio Cards
+     -->
+$('.section-works__item').click(event=>{
+    if($(event.currentTarget).hasClass('active')){
+      
+    $('.section-works__item.active').removeClass('active');  
+    } else {
+    $('.section-works__item.active').removeClass('active');
+      $(event.currentTarget).addClass('active');
+    }
+  })
